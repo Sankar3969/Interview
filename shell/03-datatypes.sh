33e98fe96562dd6cdde46835faf48690932a4dfd
sankar3969 <sankar.juvva3@gmail.com>
2025-05-29 02:05:29 +0530

    shell practice

--- shell/03-datatypes.sh
+++ shell/03-datatypes.sh
@@ -3,7 +3,7 @@ DATE=$(date "+%F")
 
 echo " THE today date is $DATE"
 
-echo " enter the Number1 "
+echo " Enter the Number1 "
 read Number1
 
 echo " enter the Number2 "
@@ -12,3 +12,10 @@ read Number2
 SUM=$(($Number1+$Number2))
 
 echo " This is the sum of numbers $SUM "
+
+if [ $SUM -gt 100 ]
+then
+echo " The sum is greater than 100 "
+else
+echo " THE sum is lessthan 100 "
+fi
