81591b4e2873acc103134ec5c5b2aa058519113e
sankar3969 <sankar.juvva3@gmail.com>
2025-05-29 01:41:04 +0530

    shell practice

---
+++ shell/02-variables.sh
@@ -0,0 +1,27 @@
+#! bin/bash
+Person1='Sankar'
+Person2='juvva'
+
+Name=$Person1
+Surname=$Person2
+echo " The Person Name is :: ${Person1} "
+echo  " The Person surname is $Person2  "
+
+
+echo 'Read the person name is from command prompt'
+read Person3
+
+echo "The Third person Name is $Person3"
+
+echo 'Read the Third person surname name without visable'
+
+read -s surname
+
+echo " the Third person surname is $surname "
+
+FRUITS=("APPLE","GRAPES")
+echo " The fruits Names are ${FRUITS[0]}"
+echo " The fruits Names are ${FRUITS[1]}"
+echo " The fruits Names are ${FRUITS[@]}"
+
+
