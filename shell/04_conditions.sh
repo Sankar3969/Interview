0d9bff5bb5ea86dbb6952857741c109456533954
sankar3969 <sankar.juvva3@gmail.com>
2025-05-29 02:58:21 +0530

    shell practice

---
+++ shell/04_conditions.sh
@@ -0,0 +1,30 @@
+#! bin/bash
+USERID=$(id -u)
+
+validate(){
+    if [ $1 -ne 0 ]
+    then
+    echo " mysql  is not installed ! Please installing now"
+     dnf install mysql -y
+     if [ $? -ne 0 ]
+     then
+     echo " mysql is not installes properly"
+     else
+     echo " mysql is installed now  "
+     fi
+    else 
+    echo " Mysql is already installed "
+    fi
+}
+if [ $USERID -ne 0 ]
+then
+echo " you don't have super user access .."
+else
+echo " you have the super user access......"
+fi
+
+dnf list installed mysql -y 
+echo " The status of mysqlstatus $? "
+mysqlstatus=$?
+
+validate $mysqlstatus
