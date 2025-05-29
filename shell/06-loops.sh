be5733cf3d5bd1bb370571eaac8e0cc3e4066acf
sankar3969 <sankar.juvva3@gmail.com>
2025-05-30 02:07:40 +0530

    committed

--- shell/06-loops.sh
+++ shell/06-loops.sh
@@ -8,15 +8,16 @@ echo "The script name is $FNAME"
 
 FILEFOLDER="/root/log_file"
 TIMESTAMP=$(date "+%F-%H-%M-%S")
-LOG_FILE="$FILEFOLDER/$FNAME-%$TIMESTAMP.log"
+LOG_FILE="$FILEFOLDER/$FNAME-$TIMESTAMP.log"
 
 echo " the log file is $LOG_FILE"
 VALIDATE(){
     if [ $USERID -ne 0 ]
     then 
-    echo " user is not super user please log in with super user"
+    echo " user is not super user please log in with super user" | tee -a $LOG_FILE
     else
-    echo " user is super user "
+    echo " user is super user " | tee -a $LOG_FILE
+    else
     fi
 }
 echo "validating the user"
