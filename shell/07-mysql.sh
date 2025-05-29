381cbc504d0f92fe3198caac0771cf419002efe9
sankar3969 <sankar.juvva3@gmail.com>
2025-05-30 03:13:22 +0530

    committed

--- shell/07-mysql.sh
+++ shell/07-mysql.sh
@@ -4,14 +4,25 @@ VALIDATE(){
     if [ $USERID -ne 0 ]
     then
     echo " user is not superuser"
+    exit 1
     else
     echo " this is super user"
+    fi
+}
+INSSTATUS(){
+    if [ $1 -ne 0 ]
+    then
+    echo " $2 is not completed properly"
+    exit 1
+    else
+    echo " $2 is completed successfully "
     fi
 }
  
 echo "validating the super user"
 VALIDATE
 
+SERVER
 
 dnf list installed mysql_server
 mysqlstatus=$?;
@@ -19,7 +30,17 @@ echo "mysql server status is $mysqlstatus "
 
 if [ $mysqlstatus -ne 0 ]
 then
-echo "mysql server not installed "
+echo "mysql server not installed!  going to install"
+
+dnf install mysql_server -y
+
+INSSTATUS $? "mysql_server"
 else
 echo "mysql server is already installed "
 fi
+
+systemctl enable mysqld
+INSSTATUS $? "mysqld enabled"
+
+systemctl start mysqld
+INSSTATUS $? "mysqld start"
